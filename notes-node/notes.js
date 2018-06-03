6c08c5b74ed947902c69a9ea9ed1b45326338da9
Pratik Dhumal <pratikshivaji.dhumal@sjsu.edu>
2018-06-03 12:14:16 -0700

    user input command line;

--- notes-node/notes.js
+++ notes-node/notes.js
@@ -2,12 +2,40 @@ console.log("Starting notes");
 
 module.exports.age=25;
 
-module.exports.addNote=()=>{
+var addNote=(title,body)=>
+{
+    console.log('Adding note',title,body);
+};
+
+var getAll=()=>
+{
+    console.log('Get all notes');
+};
+
+var getNote=(title)=>
+{
+    console.log('Gettting note:',title);
+};
+
+var removeNote=(title)=>
+{
+console.log('Removing notes',title);
+}
+
+module.exports={
+addNote,
+getAll,
+getNote,
+    removeNote
+//similar to addNote:addNote
+};
+
+/*module.exports.addNote=()=>{
 console.log('addNote');
 return 'New note';
-};
+};*/
 
-module.exports.addNumbers=(no1,no2)=>{
+/*module.exports.addNumbers=(no1,no2)=>{
 
     return no1+no2;
-};
+};*/
