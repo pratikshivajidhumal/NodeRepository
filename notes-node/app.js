0c4c20fd0df3f4b8f830a2c85e6832e5e9d31433
Pratik Dhumal <pratikshivaji.dhumal@sjsu.edu>
2018-06-03 00:45:00 -0700

    third commit require use

--- notes-node/app.js
+++ notes-node/app.js
@@ -4,8 +4,17 @@ const fs=require('fs');
 
 const os=require('os');
 
+const notes=require('./notes.js');
 
 
-var user=os.userInfo();
+var sum=notes.addNumbers(2,3);
+console.log(sum);
 
-fs.appendFileSync('greetings.txt',`Hello ${user.username}` );
+//var res=notes.addNote();
+//console.log(res);
+
+//console.log("here app:::::::");
+
+//var user=os.userInfo();
+
+//fs.appendFileSync('greetings.txt',`Hello ${user.username} you are ${notes.age}` );
---
+++ notes-node/notes.js
@@ -0,0 +1,13 @@
+console.log("Starting notes");
+
+module.exports.age=25;
+
+module.exports.addNote=()=>{
+console.log('addNote');
+return 'New note';
+};
+
+module.exports.addNumbers=(no1,no2)=>{
+
+    return no1+no2;
+};
