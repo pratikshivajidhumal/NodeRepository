d532a3acd9235a961043b18b5373e9662b189451
Pratik Dhumal <pratikshivaji.dhumal@sjsu.edu>
2018-06-03 12:12:03 -0700

    file commit;

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,23 +1,68 @@
 console.log("Starting app");
 
 const fs=require('fs');
-
-const os=require('os');
-
 const _=require('lodash');
+const yargs=require('yargs');
 
 const notes=require('./notes.js');
 
+const argv=yargs.argv;
 
-var sum=notes.addNumbers(2,3);
+
+
+var command=process.argv[2];
+
+console.log('command',command);
+console.log('process',process.argv);
+console.log('Yargs',argv);
+
+if(command==='add')
+{
+    notes.addNote(argv.title,argv.body);
+}
+else if(command==='list')
+{
+    notes.getAll();
+}
+else if(command==='read')
+{
+    notes.getNote(argv.title);
+}
+else if(command==='remove')
+{
+    notes.getNote(argv.title);
+}
+else
+{
+    console.log('not recognized');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//const os=require('os');
+
+
+//var sum=notes.addNumbers(2,3);
 //console.log("The sum is:"+sum);
 
 //console.log(_.isString(true));
 
 //console.log(_.isString('Pratik'));
 
-var filteredArray=_.uniq(['pratik',1,'pratik',1,2,3,4]);
-console.log(filteredArray);
+//var filteredArray=_.uniq([1,1,2,3,4]);
+//console.log(filteredArray);
 
 //var res=notes.addNote();
 //console.log(res);
