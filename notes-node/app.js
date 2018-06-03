4f14c801dd340f63228a8306cb9c164205b73c6b
Pratik Dhumal <pratikshivaji.dhumal@sjsu.edu>
2018-06-03 11:07:14 -0700

    deleted library;

--- notes-node/app.js
+++ notes-node/app.js
@@ -4,17 +4,20 @@ const fs=require('fs');
 
 const os=require('os');
 
-const __=require('lodash');
+const _=require('lodash');
 
 const notes=require('./notes.js');
 
 
 var sum=notes.addNumbers(2,3);
-console.log("The sum is:"+sum);
+//console.log("The sum is:"+sum);
 
-console.log(_.isString(true));
+//console.log(_.isString(true));
 
-console.log(_.isString('Pratik'));
+//console.log(_.isString('Pratik'));
+
+var filteredArray=_.uniq(['pratik',1,'pratik',1,2,3,4]);
+console.log(filteredArray);
 
 //var res=notes.addNote();
 //console.log(res);
