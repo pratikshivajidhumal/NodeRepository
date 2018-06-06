15791997cd5e78a21ad37996b3fc554d26ac8d42
Pratik Dhumal <pratikshivaji.dhumal@sjsu.edu>
2018-06-06 12:22:27 -0700

    Present JSON output in appropriate format stringify method

--- weather-app/app.js
+++ weather-app/app.js
@@ -3,5 +3,5 @@ request({
     url:'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia&key=AIzaSyD3KdI1efUsGctlAAxPiJQD03k7hBwxCfQ'
 ,json:true
 },(error,response,body)=>{
-console.log(body);
+console.log(JSON.stringify(body,undefined,2));
 })
