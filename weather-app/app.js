38bf839a64554b185f5c2685e82fe509c3aa2b05
Pratik Dhumal <pratikshivaji.dhumal@sjsu.edu>
2018-06-06 17:57:08 -0700

    weather app http response,error,body;

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,7 +1,9 @@
 const request=require('request');
 request({
     url:'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia&key=AIzaSyD3KdI1efUsGctlAAxPiJQD03k7hBwxCfQ'
-,json:true
+,json:true//Here we are setting json true to get back header in json format(application/json)
 },(error,response,body)=>{
-console.log(JSON.stringify(body,undefined,2));
+
+    console.log(`Address of this object is:${body.results[0].formatted_address}`);
+    console.log(`Lattitude is :${body.results[0].geometry.location.lat} and Longitude is ${body.results[0].geometry.location.lng}`);
 })
