979f102dfcaabb582bb50409c1dbd7d9a7c58a0a
Pratik Dhumal <pratikshivaji.dhumal@sjsu.edu>
2018-06-11 10:53:46 -0700

    how to set web server in express to show html

--- node-chat-app/server/server.js
+++ node-chat-app/server/server.js
@@ -1,13 +1,15 @@
 const express=require('express');
 const path=require('path');
-const cd=path.join(__dirname,'../public');
+const publicPath=path.join(__dirname,'../public');
 
 var expressVariable=express();
 
 //console.log(__dirname+'/../public');
 console.log(publicPath);
 
+expressVariable.use(express.static(publicPath));
+
 expressVariable.listen(3000,()=>
 {
-        console.log('Server is ready on port 3000');
+        console.log('Server is up on port 3000');
 });
